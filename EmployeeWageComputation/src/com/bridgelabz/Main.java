d9a150016a6650ce9722544189f1d90b70df3cf9
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-11 21:46:28 +0530

    Added

---
+++ EmployeeWageComputation/src/com/bridgelabz/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+public class Main {
+	public static void main(String[] args) {
+		EmployeeWageComputation obj = new EmployeeWageComputation();
+		obj.dailyWageCalculation();
+	}
+}
